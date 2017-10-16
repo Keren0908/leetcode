df48f58690809e5bec853af0952e648499e68197
keren0908 <akayayy@gmail.com>
2017-10-16 13:25:56 -0400

    55

--- src/easy.java
+++ src/easy.java
@@ -1,6 +1,10 @@
 import java.util.HashMap;
+
 import java.util.Stack;
 import java.util.List;
+import java.util.Queue;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class easy {
 
@@ -251,11 +255,11 @@ public class easy {
 	public static String countAndSay(int n) {
 
 		int i = 0;
-		String re="";
-		String s="1";
+		String re = "";
+		String s = "1";
 		while (i < n) {
-			re=s;
-			s="";
+			re = s;
+			s = "";
 			int count = 0;
 			char num = re.charAt(0);
 
@@ -268,110 +272,420 @@ public class easy {
 					count = 1;
 				}
 			}
-			s=s+Integer.toString(count)+num;
+			s = s + Integer.toString(count) + num;
 			i++;
 		}
 		return re;
 	}
-	//53.Maximum Subarray
+
+	// 53.Maximum Subarray
 	public static int maxSubArray(int[] nums) {
-		int n=nums.length;
-		int max=nums[0];
-		int alt=nums[0];
-		for(int i=0;i<n;i++) {
-			alt=Math.max(alt+nums[i], nums[i]);
-			max=Math.max(max, alt);
+		int n = nums.length;
+		int max = nums[0];
+		int alt = nums[0];
+		for (int i = 0; i < n; i++) {
+			alt = Math.max(alt + nums[i], nums[i]);
+			max = Math.max(max, alt);
 		}
 		return max;
-		
-		
+
 	}
-	
-	//58.Length of Last Word
+
+	// 58.Length of Last Word
 	public static int LengthOfLastWord_1(String s) {
-		if(s.length()==0)
+		if (s.length() == 0)
 			return 0;
-		int len=0;
-		int ind=s.length()-1;
-		while(ind>=0) {
-			while(ind>=0 && s.charAt(ind)==' ')
+		int len = 0;
+		int ind = s.length() - 1;
+		while (ind >= 0) {
+			while (ind >= 0 && s.charAt(ind) == ' ')
 				ind--;
-			while(ind>=0 && s.charAt(ind)!=' ') {
+			while (ind >= 0 && s.charAt(ind) != ' ') {
 				ind--;
 				len++;
 			}
 			break;
 		}
 		return len;
-				
-			
-		}
+
+	}
+
 	public static int LengthOfLastWord(String s) {
-		return s.trim().length()-s.trim().lastIndexOf(' ')-1;
+		return s.trim().length() - s.trim().lastIndexOf(' ') - 1;
 	}
-	
-	//66.plus one
+
+	// 66.plus one
 	public static int[] plusOne_error(int[] digits) {
-		String digit="";
-		for(int i=0;i<digits.length;i++) {
-			digit=digit+digits[i];
+		String digit = "";
+		for (int i = 0; i < digits.length; i++) {
+			digit = digit + digits[i];
 		}
-		String d=Long.toString(Long.parseLong(digit)+1);
-		int[] re= new int[d.length()];
-		for(int j=0;j<d.length();j++) {
-			re[j]=Integer.parseInt(String.valueOf(d.charAt(j)));
-		
+		String d = Long.toString(Long.parseLong(digit) + 1);
+		int[] re = new int[d.length()];
+		for (int j = 0; j < d.length(); j++) {
+			re[j] = Integer.parseInt(String.valueOf(d.charAt(j)));
+
 		}
-		
+
 		return re;
-		
-		
+
 	}
-	
+
 	public static int[] plusOne(int[] digits) {
-		int n=digits.length;
-		
-		int remainder=(digits[n-1]+1)%10;
-		int quotient=(digits[n-1]+1)/10;
-		digits[n-1]=remainder;
-		
-		for(int i=n-2;i>=0;i--) {
-			digits[i]+=quotient;
-			remainder=digits[i]%10;
-			quotient=digits[i]/10;
-			digits[i]=remainder;
+		int n = digits.length;
+
+		int remainder = (digits[n - 1] + 1) % 10;
+		int quotient = (digits[n - 1] + 1) / 10;
+		digits[n - 1] = remainder;
+
+		for (int i = n - 2; i >= 0; i--) {
+			digits[i] += quotient;
+			remainder = digits[i] % 10;
+			quotient = digits[i] / 10;
+			digits[i] = remainder;
 		}
-		if(quotient==0)
+		if (quotient == 0)
 			return digits;
 		else {
-			int[] new_digits=new int[n+1];
-			for(int i=0;i<n;i++)
-				new_digits[i+1]=digits[i];
-			new_digits[0]=quotient;
+			int[] new_digits = new int[n + 1];
+			for (int i = 0; i < n; i++)
+				new_digits[i + 1] = digits[i];
+			new_digits[0] = quotient;
 			return new_digits;
 		}
-			
-		
-		
+
 	}
+
 	public static int[] plusOne_1(int[] digits) {
-		int n=digits.length;
-		for(int i=n-1;i>=0;i--) {
-			if(digits[i]<9) {
+		int n = digits.length;
+		for (int i = n - 1; i >= 0; i--) {
+			if (digits[i] < 9) {
 				digits[i]++;
 				return digits;
 			}
-			digits[i]=0;
-			
+			digits[i] = 0;
+
 		}
-		int[] new_digits=new int[n+1];
-		new_digits[0]=1;
+		int[] new_digits = new int[n + 1];
+		new_digits[0] = 1;
 		return new_digits;
-		
+
+	}
+
+	// 67.add binary
+	public static String addBinary(String a, String b) {
+		if (a.length() < b.length()) {
+			for (int i = 0; i < b.length() - a.length(); i++)
+				a = "0" + a;
+		} else if (a.length() > b.length()) {
+			for (int i = 0; i < a.length() - b.length(); i++)
+				b = "0" + b;
+		}
+		int[] sa = new int[a.length()];
+		int[] sb = new int[b.length()];
+
+		for (int i = 0; i < a.length(); i++) {
+			sa[i] = Character.getNumericValue(a.charAt(i));
+			sb[i] = Character.getNumericValue(b.charAt(i));
+		}
+
+	}
+
+	public static String addBinary_1(String a, String b) {
+		StringBuilder sb = new StringBuilder();
+		int i = a.length() - 1;
+		int j = b.length() - 1;
+		int carry = 0;
+		while (i >= 0 || j >= 0) {
+			int sum = carry;
+			if (i >= 0)
+				sum += a.charAt(i--) - '0';
+			if (j >= 0)
+				sum += b.charAt(j--) - '0';
+			sb.append(sum % 2);
+			carry = sum / 2;
+		}
+		if (carry != 0)
+			sb.append(1);
+		return sb.reverse().toString();
+
+	}
+
+	// 83 remove duplicates from sorted list
+	public static ListNode deleteDuplicates(ListNode head) {
+		if (head == null || head.next == null)
+			return head;
+		head.next = deleteDuplicates(head.next);
+		return head.val == head.next.val ? head.next : head;
+
+	}
+
+	public static ListNode deleteDuplicates(ListNode head) {
+
+		ListNode list = head;
+		while (list != null) {
+			if (list.next == null) {
+				break;
+			}
+			if (list.val == list.next.val)
+				list.next = list.next.next;
+			else
+				list = list.next;
+		}
+		return head;
+	}
+
+	// 88.Merge Sorted Array
+	public static void merge(int[] nums1, int m, int[] nums2, int n) {
+		int k = m + n - 1;
+		int i = m - 1;
+		int j = n - 1;
+		while (i >= 0 || j >= 0) {
+			if (i < 0)
+				nums1[k--] = nums2[j--];
+			if (j < 0)
+				nums1[k--] = nums1[i--];
+			if (i >= 0 && j >= 0)
+				nums1[k--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+		}
+
+	}
+
+	// 100.Same Tree
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public static boolean isSameTree(TreeNode p, TreeNode q) {
+		Stack<TreeNode> stack_p = new Stack<TreeNode>();
+		Stack<TreeNode> stack_q = new Stack<TreeNode>();
+		stack_p.push(p);
+		stack_q.push(q);
+		while (!stack_p.isEmpty() && !stack_q.isEmpty()) {
+			TreeNode pp = stack_p.pop();
+			TreeNode qq = stack_q.pop();
+			if (pp.val != qq.val)
+				return false;
+			if (pp.left != null)
+				stack_p.push(pp.left);
+			if (qq.left != null)
+				stack_q.push(qq.left);
+			if (stack_p.size() != stack_q.size())
+				return false;
+			if (pp.right != null)
+				stack_p.push(pp.right);
+			if (qq.right != null)
+				stack_q.push(qq.right);
+			if (stack_p.size() != stack_q.size())
+				return false;
+		}
+		return stack_p.size() == stack_q.size();
+
+	}
+
+	public static boolean isSameTree_1(TreeNode p, TreeNode q) {
+		if (p == null && q == null)
+			return true;
+		if (p == null || q == null)
+			return false;
+		if (p.val == q.val) {
+			return isSameTree_1(p.left, q.left) && isSameTree_1(p.right, q.right);
+		}
+		return true;
+	}
+
+	// 101.Symmetric Tree
+	public static boolean isSymmetric(TreeNode root) {
+		if (root == null)
+			return true;
+		Stack<TreeNode> left = new Stack<TreeNode>();
+		Stack<TreeNode> right = new Stack<TreeNode>();
+		if (root.left != null)
+			left.push(root.left);
+		if (root.right != null)
+			right.push(root.right);
+		while (!left.isEmpty() && !right.isEmpty()) {
+			TreeNode l = left.pop();
+			TreeNode r = right.pop();
+			if (l.val != r.val)
+				return false;
+			if (l.left != null)
+				left.push(l.left);
+			if (r.right != null)
+				right.push(r.right);
+			if (left.size() != right.size())
+				return false;
+			if (l.right != null)
+				left.push(l.right);
+			if (r.left != null)
+				right.push(r.left);
+			if (left.size() != right.size())
+				return false;
+		}
+		return left.size() == right.size();
+
+	}
+
+	// 104.Maximum Depth of Binary Tree
+	public static int maxDepth(TreeNode root) {
+		if (root == null)
+			return 0;
+		int l = maxDepth(root.left);
+		int r = maxDepth(root.right);
+
+		return l > r ? l + 1 : r + 1;
+
+	}
+
+	// 107.Binary Tree level Order Traversal II
+	public List<List<Integer>> levelOrderBottom(TreeNode root) {
+		List<List<Integer>> list = new LinkedList<List<Integer>>();
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+		if (root == null)
+			return list;
+		int nCount = 1;// 记录每一层的node树
+		int nDepth = 0;// 记录深度
+
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			List<Integer> sublist = new LinkedList<Integer>();
+			while (nCount != 0) {
+				TreeNode temp = queue.peek();
+				sublist.add(queue.poll().val);
+				nCount--;
+				if (temp.left != null)
+					queue.offer(temp.left);
+				if (temp.right != null)
+					queue.offer(temp.right);
+			}
+			if (nCount == 0) {
+				nDepth++;
+				nCount = queue.size();
+				list.add(0, sublist);
+
+			}
+		}
+		return list;
+
+	}
+
+	// 108.convert sorted array to binary search tree
+	public static TreeNode sortedArrayToBST(int[] nums) {
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+		int n = nums.length;
+		if (n == 0)
+			return null;
+		int i = 1;
+
+		TreeNode root = new TreeNode(nums[0]);
+
+		queue.offer(root);
+		while (i < n) {
+			TreeNode temp = queue.poll();
+			TreeNode left = new TreeNode(nums[i++]);
+			temp.left = left;
+			queue.offer(left);
+			TreeNode right = new TreeNode(nums[i++]);
+			temp.right = right;
+			queue.offer(right);
+		}
+		return root;
+
+	}
+
+	// 110.Balanced Binary Tree
+	public static boolean isBalanced(TreeNode root) {
+		if (root == null)
+			return true;
+		int lh = NodeHeight(root.left);
+		int rh = NodeHeight(root.right);
+		if (Math.abs(lh - rh) > 1)
+			return false;
+		return isBalanced(root.left) && isBalanced(root.right);
+
+	}
+
+	public static int NodeHeight(TreeNode root) {
+		if (root == null)
+			return 0;
+		if (root.left == null && root.right == null)
+			return 1;
+		int lh = NodeHeight(root.left);
+		int rh = NodeHeight(root.right);
+		return Math.max(lh, rh) + 1;
+
+	}
+
+	// 111.Minimum depth of binary tree
+
+	// dfs depth first search
+	public int minDepth(TreeNode root) {
+		if (root == null)
+			return 0;
+		if (root.left == null && root.right == null)
+			return 1;
+		int lt = Integer.MAX_VALUE;
+		int rt = Integer.MAX_VALUE;
+		if (root.left != null)
+			lt = minDepth(root.left);
+		if (root.right != null)
+			rt = minDepth(root.right);
+		return Math.min(lt, rt) + 1;
+	}
+
+	// bfs breadth first search
+	public int minDepth_1(TreeNode root) {
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		int nCount = 1;
+		int nDepth = 0;
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			TreeNode temp = queue.poll();
+			nCount--;
+
+			if (temp.left != null)
+				queue.offer(temp.left);
+			if (temp.right != null)
+				queue.offer(temp.right);
+
+			if (temp.left == null && temp.right == null) {
+				nDepth++;
+				return nDepth;
+			}
+
+			if (nCount == 0) {
+				nCount = queue.size();
+				nDepth++;
+			}
+		}
+		return nDepth;
+
 	}
 	
-		
+	//665.Non-decreasing array
+	public static boolean checkPossibility(int[] nums) {
+		int n=nums.length;
+		if(n==0 || n==1 || n==2) return true;
+		int count=0;
+		for(int i=0;i+1<n;i++) {
+			if(nums[i]>nums[i+1]) {
+				count++;
+				if(i>0 && nums[i+1]>nums[i-1]) nums[i]=nums[i+1];
+				else nums[i+1]=nums[i];
+			}
+		}
+		return count<=1;
 	
+		
+	}
 
 	public static void main(String[] args) {
 		/*
@@ -387,11 +701,8 @@ public class easy {
 		// String a="(){}[]";
 		// System.out.println(isValid(a));
 		// int[] nums= {1,3,5,6};
-		int[] a= {9,8,7,6,5,4,3,2,1,0};
-		
-		int[] b=plusOne_1(a);
-		for(int i:b)
-			System.out.println(i);
+		int[] nums= {5,4,2,3,3};
+		System.out.println(checkPossibility(nums));
 
 	}
 
